fa1ff87216cf1d9f8a0e5f0daeecbabc4442fb38
schubert-da <schubertdeabreu2@gmail.com>
2025-03-31 12:35:10 +0530

    add chart images to parallax scene

--- src/lib/components/ParallaxScene/ParallaxScene.svelte
+++ src/lib/components/ParallaxScene/ParallaxScene.svelte
@@ -3,15 +3,20 @@
 	import ArtLearningImage from '$lib/assets/images/life-viz/art-learning.png';
 	import DatavizLearningImage from '$lib/assets/images/life-viz/dataviz.png';
 	import DatavizDevelopmentImage from '$lib/assets/images/life-viz/dataviz-development.png';
+
+	import StudyWorkImageViz from '$lib/assets/images/life-viz/study-work-viz.png';
+	import ArtLearningImageViz from '$lib/assets/images/life-viz/art-learning-viz.png';
+	import DatavizLearningImageViz from '$lib/assets/images/life-viz/dataviz-viz.png';
+	import DatavizDevelopmentImageViz from '$lib/assets/images/life-viz/dataviz-development-viz.png';
 	import CareerBackground from '$lib/assets/images/life-viz/career-layer-bg.png';
 
 	import { scrollerParams } from '$lib/utils/stores';
 
 	export let layerConfig = [
-		{ src: StudyWorkImage, order: 1, speed: 0.25 },
-		{ src: ArtLearningImage, order: 2, speed: 0.3 },
-		{ src: DatavizLearningImage, order: 3, speed: 0.35 },
-		{ src: DatavizDevelopmentImage, order: 4, speed: 0.3 }
+		{ src: StudyWorkImage, chartSrc: StudyWorkImageViz, order: 1, speed: 0.25 },
+		{ src: ArtLearningImage, chartSrc: ArtLearningImageViz, order: 2, speed: 0.3 },
+		{ src: DatavizLearningImage, chartSrc: DatavizLearningImageViz, order: 3, speed: 0.35 },
+		{ src: DatavizDevelopmentImage, chartSrc: DatavizDevelopmentImageViz, order: 4, speed: 0.3 }
 	];
 	export const cardIndex = 0;
 
@@ -28,12 +33,13 @@
 
 <div class="scene-container">
 	<img class="placeholder-image" src={CareerBackground} alt="Layer" />
-	{#each layerConfig as { src, order, speed }, i}
+	{#each layerConfig as { src, chartSrc, order, speed }, i}
 		<div
 			class="layer"
 			style="--order: {order}; --yTranslate: {calculateYTranslate($scrollerParams, speed)}%;"
 		>
-			<img {src} alt="Layer {i + 1}" />
+			<img class="art-image" {src} alt="Layer {i + 1}" />
+			<img class="chart-image" src={chartSrc} alt="Layer {i + 1}" />
 		</div>
 	{/each}
 </div>
@@ -73,6 +79,19 @@
 				width: 100%;
 				height: 100%;
 				object-fit: contain;
+
+				opacity: 1;
+			}
+
+			img.chart-image {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+
+				z-index: 1;
+				opacity: 0;
 			}
 		}
 	}
