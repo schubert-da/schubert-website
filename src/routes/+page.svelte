f5a9604d24a0e9b115a1ed18af353b5783baff28
schubert-da <schubertdeabreu2@gmail.com>
2025-05-20 21:53:17 +0530

    fix scrollerParams setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,39 @@
 	import FeaturedWork from '$lib/components/Works/FeaturedWork.svelte';
 	import { scrollerParams, sectionHeights } from '$lib/utils/stores';
 	import Scroller from '@sveltejs/svelte-scroller';
+
 	let index, offset, progress;
 	$: if (index || offset || progress) {
 		scrollerParams.set({ index, offset, progress });
 	}
 </script>
 
-<Scroller top={0.1} threshold={0.35} bind:index bind:offset bind:progress>
+<Scroller
+	top={0.1}
+	threshold={0.35}
+	query="div.foreground-section"
+	bind:index
+	bind:offset
+	bind:progress
+>
 	<div slot="background"></div>
 
 	<div slot="foreground">
-		<div class="intro-section-container" bind:clientHeight={$sectionHeights.home}>
+		<div
+			class="foreground-section intro-section-container"
+			bind:clientHeight={$sectionHeights.home}
+		>
 			<HeroSection></HeroSection>
 		</div>
 
-		<div class="process-section-container">
+		<div class="foreground-section process-section-container">
 			<ProcessSection></ProcessSection>
 		</div>
 
-		<div class="works-section-container" bind:clientHeight={$sectionHeights.works}>
+		<div
+			class="foreground-section works-section-container"
+			bind:clientHeight={$sectionHeights.works}
+		>
 			<WorkSection></WorkSection>
 		</div>
 	</div>
