c47b3018bec5e8f097c5fb6a182a76ffb2b19c14
schubert-da <schubertdeabreu2@gmail.com>
2025-05-27 06:52:27 +0530

    adds wave city grid component

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -4,16 +4,9 @@
 	import LinkedInIcon from '$lib/assets/images/icons/linkedin.svg?raw';
 	import EmailIcon from '$lib/assets/images/icons/email.svg?raw';
 	import BlueskyIcon from '$lib/assets/images/icons/bluesky.svg?raw';
-
-	import TileBlank from '$lib/assets/images/wave-playground/tile-blank.jpg';
-	import TileDeadend from '$lib/assets/images/wave-playground/tile-deadend.jpg';
-	import TileTJunction from '$lib/assets/images/wave-playground/tile-tjunction.jpg';
-	import TileCurve from '$lib/assets/images/wave-playground/tile-curve.jpg';
-
-	const tiles = [TileBlank, TileDeadend, TileTJunction, TileCurve];
+	import WaveCityGrid from './WaveCityGrid/WaveCityGrid.svelte';
 
 	let screenWidth = $state(1000);
-	let gridWidth = $state(500);
 </script>
 
 <svelte:window bind:innerWidth={screenWidth} />
@@ -63,19 +56,8 @@
 			</div>
 		</div>
 
-		<div class="game-grid" bind:clientWidth={gridWidth}>
-			{#each Array(15).fill() as _, i}
-				{@const tile = tiles[Math.floor(Math.random() * tiles.length)]}
-				{@const tileRotation = Math.floor(Math.random() * 4) * 90}
-				<div class="tile" style:--tile-width="{gridWidth / 5 - 0.05}px">
-					<img
-						src={tile}
-						alt="tile"
-						style:width="{gridWidth / 5 - 0.05}px"
-						style="transform: rotate({tileRotation}deg);"
-					/>
-				</div>
-			{/each}
+		<div class="game-grid-container">
+			<WaveCityGrid></WaveCityGrid>
 		</div>
 	</div>
 </footer>
@@ -172,23 +154,6 @@
 						font-size: var(--font-size--1);
 						line-height: 1.2;
 					}
-				}
-			}
-
-			.game-grid {
-				display: flex;
-				flex-direction: row;
-				flex-wrap: wrap;
-
-				max-width: 1100px;
-				margin: 4rem auto 0 auto;
-				border: 4px solid #666;
-				border-radius: 6px;
-
-				.tile {
-					width: var(--tile-width);
-					height: var(--tile-width);
-					border: 1px solid #666;
 				}
 			}
 		}
---
+++ src/lib/components/Footer/WaveCityGrid/WaveCityGrid.svelte
@@ -0,0 +1,46 @@
+<script>
+	import TileBlank from '$lib/assets/images/wave-playground/tile-blank.jpg';
+	import TileDeadend from '$lib/assets/images/wave-playground/tile-deadend.jpg';
+	import TileTJunction from '$lib/assets/images/wave-playground/tile-tjunction.jpg';
+	import TileCurve from '$lib/assets/images/wave-playground/tile-curve.jpg';
+
+	let gridWidth = $state(500);
+	let tileWidth = $derived(gridWidth / 5 - 0.01);
+
+	const tiles = [TileBlank, TileDeadend, TileTJunction, TileCurve];
+	$inspect(gridWidth, tileWidth);
+</script>
+
+<div class="game-grid" bind:clientWidth={gridWidth}>
+	{#each Array(15).fill() as _, i}
+		{@const tile = tiles[Math.floor(Math.random() * tiles.length)]}
+		{@const tileRotation = Math.floor(Math.random() * 4) * 90}
+		<div class="tile" style:--tile-width="{tileWidth}px">
+			<img
+				src={tile}
+				alt="tile"
+				style:width="{tileWidth}px"
+				style="transform: rotate({tileRotation}deg);"
+			/>
+		</div>
+	{/each}
+</div>
+
+<style lang="scss">
+	.game-grid {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+
+		max-width: 1100px;
+		margin: 4rem auto 0 auto;
+		border: 4px solid #666;
+		border-radius: 6px;
+
+		.tile {
+			width: var(--tile-width);
+			height: var(--tile-width);
+			border: 1px solid #666;
+		}
+	}
+</style>
