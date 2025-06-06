06025193507ccbad532d9b461dfc1e246f2575e2
schubert-da <schubertdeabreu2@gmail.com>
2025-06-06 10:06:37 +0530

    gsap animations for footer grid

--- src/lib/components/Footer/WaveCityGrid/WaveCityGrid.svelte
+++ src/lib/components/Footer/WaveCityGrid/WaveCityGrid.svelte
@@ -7,6 +7,7 @@
 	import { TILE_SET } from './tileSet';
 	import { collapseTiles, initTiles } from './WaveFunction';
 	import { onMount } from 'svelte';
+	import gsap from 'gsap';
 
 	const NUM_ROWS = 3;
 	const NUM_COLS = 5;
@@ -18,74 +19,142 @@
 	let tiles = $state([]);
 	let currentRow = 0;
 
-	onMount(() => {
-		tiles = initTiles(tiles, NUM_ROWS, NUM_COLS, 3);
-		tiles = collapseTiles(tiles, currentRow, NUM_ROWS, NUM_COLS);
+	let tilesList = $state({});
 
-		currentRow = 2;
+	onMount(async () => {
+		tiles = await initTiles(tiles, NUM_ROWS, NUM_COLS, 1);
+		tiles = await collapseTiles(tiles, currentRow, NUM_ROWS, NUM_COLS);
+
+		tiles.forEach((tile, index) => {
+			const tileElement = tilesList[`${currentRow * NUM_COLS + index}`];
+			gsap.to(tileElement, {
+				opacity: 1,
+				duration: 0.5,
+				ease: 'power1.inOut',
+				delay: DELAY * tile.order
+			});
+		});
+
+		currentRow = 0;
+		await animateNextTiles(currentRow);
 
 		const interval = setInterval(
-			() => {
-				tiles = initTiles(tiles, NUM_ROWS, NUM_COLS, 2);
-				tiles = collapseTiles(tiles, currentRow, NUM_ROWS, NUM_COLS);
+			async () => {
 				currentRow += 2;
-				console.log('tiles', tiles);
+				animateNextTiles(currentRow);
 			},
-			currentRow === 2 ? DELAY * NUM_COLS * 3 * 1000 : DELAY * NUM_COLS * 2 * 1000
+			DELAY * (NUM_COLS * 2) * 1000 + DELAY * 1000
 		);
 
 		return () => clearInterval(interval);
 	});
 
-	function computeDelay(tileOrder) {
-		if (tileOrder < NUM_COLS * 3) {
-			return DELAY * tileOrder + DELAY;
+	async function animateNextTiles(currentRow) {
+		const addedDelay = currentRow === 0 ? DELAY * 5 : 0;
+
+		console.log('Animating next tiles for row:', currentRow);
+		const timeline = gsap.timeline({ paused: true });
+
+		tiles = await initTiles(tiles, NUM_ROWS, NUM_COLS, 2);
+		tiles = await collapseTiles(tiles, currentRow, NUM_ROWS, NUM_COLS);
+		console.log('Tiles after collapse:', tiles);
+		let currentTiles = await tiles.slice((currentRow + 1) * NUM_COLS, (currentRow + 3) * NUM_COLS);
+		console.log('currentTiles:', currentTiles);
+
+		currentTiles
+			.slice()
+			.sort((a, b) => a.order - b.order)
+			.forEach((tile, index) => {
+				const tileElement = tilesList[`${(currentRow + 1) * NUM_COLS + index}`];
+				timeline.to(
+					tileElement,
+					{
+						opacity: 1,
+						duration: 0.5,
+						ease: 'power1.inOut',
+						delay: DELAY
+					},
+					computeDelay(currentRow, tile.order, addedDelay)
+				);
+			});
+
+		timeline.to(
+			'.game-grid',
+			{
+				y: `-=${tileWidth * 2}px`,
+				duration: 0.5,
+				ease: 'power1.inOut',
+				delay: DELAY
+			},
+			'+=0.1'
+		);
+
+		timeline.play();
+	}
+
+	function computeDelay(currentRow, order, addedDelay = 0) {
+		if (currentRow === 0 && order === 0) {
+			return `+=${DELAY * 5}`;
+		} else if (order === 0) {
+			return `+=${DELAY}`;
 		} else {
-			return DELAY * (tileOrder % (NUM_COLS * 2)) + DELAY;
+			return '<';
 		}
 	}
 </script>
 
-<div class="game-grid" bind:clientWidth={gridWidth}>
-	{#each Array(Math.floor(tiles.length / NUM_COLS)) as _, rowIndex}
-		<div class="row">
-			{#each Array(NUM_COLS) as _, colIndex}
-				{@const tile = tiles[rowIndex * NUM_COLS + colIndex]}
-				<div
-					class="tile"
-					style:--tile-width="{tileWidth}px"
-					style="transition-delay: {0}s;"
-					class:collapsed={tile?.collapsed}
-					class:left={tile?.connections?.left}
-					class:right={tile?.connections?.right}
-					class:top={tile?.connections?.top}
-					class:bottom={tile?.connections?.bottom}
-				>
-					{#if tile.image}
-						<img
-							style:--transition-delay="{computeDelay(tile.order)}s"
-							style:--tile-rotation="{tile.image.rotation}deg"
-							class="tile-image"
-							src="/tiles/{tile.image.paths}"
-							alt="tile"
-						/>
-					{/if}
-				</div>
-			{/each}
-		</div>
-	{/each}
+<!-- <div class="grid-window"> -->
+<div class="grid-window" style:height="{tileWidth * 3}px">
+	<div class="game-grid" bind:clientWidth={gridWidth}>
+		{#each Array(Math.floor(tiles.length / NUM_COLS)) as _, rowIndex}
+			<div class="row">
+				{#each Array(NUM_COLS) as _, colIndex}
+					{@const tile = tiles[rowIndex * NUM_COLS + colIndex]}
+					<div
+						class="tile"
+						style:--tile-width="{tileWidth}px"
+						class:collapsed={tile?.collapsed}
+						class:left={tile?.connections?.left}
+						class:right={tile?.connections?.right}
+						class:top={tile?.connections?.top}
+						class:bottom={tile?.connections?.bottom}
+						bind:this={tilesList[rowIndex * NUM_COLS + colIndex]}
+					>
+						{#if tile?.image}
+							<img
+								style:--tile-rotation="{tile?.image?.rotation}deg"
+								class="tile-image"
+								src="/tiles/{tile?.image?.paths}"
+								alt="tile"
+							/>
+						{/if}
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style lang="scss">
+	.grid-window {
+		position: relative;
+		max-width: 1100px;
+		margin: 4rem auto 0 auto;
+		border: 4px solid #666;
+		border-radius: 6px;
+		overflow: hidden;
+	}
+
 	.game-grid {
+		// position: absolute;
+		top: 0;
+		left: 0;
+
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
 
 		max-width: 1100px;
-		margin: 4rem auto 0 auto;
-		border: 4px solid #666;
-		border-radius: 6px;
 
 		.row {
 			display: flex;
@@ -96,6 +165,7 @@
 			width: var(--tile-width);
 			height: var(--tile-width);
 			border: 1px solid #666;
+			opacity: 0;
 
 			img {
 				width: 100%;
@@ -103,8 +173,7 @@
 				object-fit: cover;
 
 				transform: rotate(var(--tile-rotation));
-				opacity: 0;
-				animation: fadeIn 0.5s forwards var(--transition-delay);
+				opacity: 1;
 			}
 		}
 	}
